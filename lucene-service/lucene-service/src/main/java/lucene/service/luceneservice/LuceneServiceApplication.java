7a5dcec2109ff03965f787317844acb297e7a77a
chee <xiong726@uwm.edu>
2024-03-23 17:47:13 -0500

    add springboot project

---
+++ lucene-service/lucene-service/src/main/java/lucene/service/luceneservice/LuceneServiceApplication.java
@@ -0,0 +1,13 @@
+package lucene.service.luceneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuceneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuceneServiceApplication.class, args);
+	}
+
+}
