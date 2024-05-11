27ccff9cd0e1e4b77258f850ad2fb4079c75b5ef
chee <xiong726@uwm.edu>
2024-05-11 05:15:29 +0000

    add content components for DDI page

--- ddi-ui/vite.config.js
+++ ddi-ui/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      // Use polling instead of native watchers
+      // Useful for situations like running inside a Docker container
+      usePolling: true,
+    }
+  },
 })
