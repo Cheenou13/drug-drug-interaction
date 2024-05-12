e874d6b85ddfb8867fe81a159114ca6e68413908
chee <xiong726@uwm.edu>
2024-05-12 06:44:56 +0000

    finished styled search box

--- ddi-ui/src/components/RxForm.jsx
+++ ddi-ui/src/components/RxForm.jsx
@@ -1,11 +1,16 @@
 
-
 const RxForm = () => {
 
     return (
         <>
             <div className="form-row interax-form-row">
-
+                <form className="name-search-form" id="drug-interaction" action="/ddi#results" acceptCharset="UTF-8" method="post">
+                    <input type="hidden" name="authenticity_token" value={"ddi-12345"} autoComplete="off" ></input>
+                    <div className="drug-search-box">
+                        <h6 className='interax-header'>Add drug to check for DDI</h6>
+                        <div className="drug-search"></div>
+                    </div>
+                </form>
             </div>
         </>
     )
